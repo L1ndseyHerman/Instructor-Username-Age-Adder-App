49272aa3f348681a18ebfc57dd82655d19ada569
L1ndseyHerman <lherman21@jcu.edu>
2021-09-04 21:45:03 -0400

    Caught up to vid95 with the UsersList.

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
@@ -11,8 +11,33 @@ import Button from '../UI/Button';
 //  Ahh, he does use "number" for the type even though that comes w its own error messages.
 const AddUser = props => {
 
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        //  THIS IS DIF THAN WHAT U DID, WHAT U SHOULD HAVE DONE!!
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0)
+        {
+            return;
+        }
+        //  Don't need "else if" bec would have returned already from the first one if that true.
+        //  Will the program crash on the "+" if enteredAge isn't a number? Like if it's "a"?
+        if (+enteredAge < 1)
+        {
+            return;
+        }
+        props.onAddUser(enteredUsername, enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
+    };
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     };
 
     //  addUserHandler = a pointer to that function.
@@ -22,9 +47,9 @@ const AddUser = props => {
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" />
+                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" />
+                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
